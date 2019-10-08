d1e7e56f95b05c502edda943fd72dcca6a929dc2
Josh Deese <joshmdeese@gmail.com>
2019-10-08 10:03:01 -0400

    updated -h and --help tests to use ava

--- test/index.js
+++ test/index.js
@@ -11,5 +11,10 @@ test('shows up help message without any args', async t => {
 	t.snapshot(stdout);
 });
 
-
+['-h', '--help'].forEach(function(i){
+  test('shows help message with arg ' + i, async t => {
+    const { stdout } = await execa(rootCommand, [i]);
+    t.snapshot(stdout);
+  });
+});
 
